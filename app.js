c51cc8529d4ad0a32536ece76f281b97f2fdb908
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2023-06-02 14:48:28 +0300

    fix

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-const express = require("express"); // екземпляр express програми.
-const product = require("./date");
-const dataProduc = require("./db/products.json");
-const fs = require("fs/promises"); // fs - пакет для роботи з файлами, читати, записувати
-const moment = require("moment");
+const app = require("express").express(); // екземпляр express програми.
+
 const cors = require("cors"); // cors - для запитів з ішого браузера
 const productRouter = require("./routes/api/products");
-
-const app = express(); // app - веб сервер
 
 app.use(cors()); // для запитів з ішого браузера
 app.use(express.json()); // Парсер JSON щоб інтерпретувати значення req.body як об'єкт замість рядка
@@ -20,11 +15,3 @@ app.use((req, res) => {
 });
 
 app.listen(3000, () => console.log("Server running"));
-
-// app.use(async (req, res, next) => {
-//   const { method, url } = req;
-//   const date = moment().format("DD-MM-YY_hh:mm:ss");
-//   await fs.appendFile("server.log", `\n${method},${url},${date}`);
-
-//   next();
-// });
