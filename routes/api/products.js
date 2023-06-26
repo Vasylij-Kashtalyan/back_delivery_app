a8dce4b30e6c1ec6ca1ea2ef334b942f8194bef7
Vasylij Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2023-06-26 12:19:30 +0300

    add Counter

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Joi = require("joi")
+const Joi = require("joi");
 
 const productsModel = require("../../models/models");
 const { createError } = require("../../helpers");
@@ -7,48 +7,47 @@ const { createError } = require("../../helpers");
 const router = express.Router(); //Router() - для створення модульних,монтованих обробників маршрутів
 
 const productSchema = Joi.object({
-  name: Joi.string().required(),
-  description: Joi.string().required(),
-  price: Joi.number().required(),
-
-})
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().required(),
+});
 
 router.get("/", async (req, res, next) => {
-  try {
-    const result = await productsModel.getAll();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const result = await productsModel.getAll();
+        res.json(result);
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await productsModel.getById(id);
+    try {
+        const { id } = req.params;
+        const result = await productsModel.getById(id);
 
-    if (!result) {
-      throw createError(404);
+        if (!result) {
+            throw createError(404);
+        }
+
+        res.json(result);
+    } catch (error) {
+        next(error);
     }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
 });
 
 router.post("/", async (req, res, next) => {
-  try {
-    const {error} = productSchema.validate(req.body)
-if(error){
-  throw createError(400, error.message)
-}
+    try {
+        const { error } = productSchema.validate(req.body);
+        if (error) {
+            throw createError(400, error.message);
+        }
 
-    const results = await productsModel.addProduct(req.body)
-    res.status(201).json(results)
-  } catch (error) {
-    next(error)
-  }
+        const results = await productsModel.addProduct(req.body);
+        res.status(201).json(results);
+    } catch (error) {
+        next(error);
+    }
 });
 
 // router.delete("/:id", (req, res) => {
