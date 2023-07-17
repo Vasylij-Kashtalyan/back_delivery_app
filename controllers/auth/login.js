fd8a771694d8eabb90ceae1bdb83c9d836b63e60
Vasylij Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2023-07-17 20:41:23 +0300

    jwt token

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -2,6 +2,18 @@ const { User, loginSchema } = require("../../models/user");
 const { createError } = require("../../helpers");
 const bcrypt = require("bcryptjs");
 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { SECRET_KEY } = process.env;
+
+const payload = {
+    id: "64b3cab4dab0d119a9f7a901",
+};
+
+const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
+console.log(token);
+
 const login = async (req, res) => {
     const { error } = loginSchema.validate(req.body);
 
